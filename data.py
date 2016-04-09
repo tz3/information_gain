fe2873cd632e2401616b2d220d80eb5b8b5963bd
Akhmetov Rinat <appvshare@gmail.com>
2016-04-09 07:28:03 +0300

    model for toys meta

---
+++ data.py
@@ -0,0 +1,11 @@
+class Data(object):
+    def __init__(find_name):
+        #load some
+        pass
+
+    def find_by_asin(asin):
+        return [
+                {'asin': '0000191639', 'description': "Three Dr. Suess' Puzzles: Green Eggs and Ham, Favorite Friends, and One Fish Two Fish Red Fish Blue Fish", 'title': 'Dr. Suess 19163 Dr. Seuss Puzzle 3 Pack Bundle', 'price': 37.12, 'salesRank': {'Toys & Games': 612379}, 'imUrl': 'http://ecx.images-amazon.com/images/I/414PLROXy-L._SY300_.jpg', 'brand': 'Dr. Seuss', 'categories': [['Toys & Games', 'Puzzles', 'Jigsaw Puzzles']]}
+                {'asin': '0005069491', 'salesRank': {'Toys & Games': 576683}, 'imUrl': 'http://ecx.images-amazon.com/images/I/51z4JDBCnAL._SY300_.jpg', 'categories': [['Toys & Games']], 'title': 'Nursery Rhymes Felt Book'}
+                {'asin': '0076561046', 'description': 'Learn Fractions Decimals Percents using flash cards.', 'title': 'Fraction Decimal Percent Card Deck', 'imUrl': 'http://ecx.images-amazon.com/images/I/51ObabPu3tL._SY300_.jpg', 'related': {'also_viewed': ['0075728680']}, 'salesRank': {'Toys & Games': 564211}, 'categories': [['Toys & Games', 'Learning & Education', 'Flash Cards']]}
+            ]
